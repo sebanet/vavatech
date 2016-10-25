70288e3e0b4362b2925af386d69c87a00b231585
sebian <informatyk@interia.com>
2016-10-25 10:18:49 +0200

    java2

---
+++ src/Cwiczenie4.java
@@ -0,0 +1,8 @@
+public class Cwiczenie4 {
+    public static void main(String[] args){
+        int silniaZ5 = silnia.silniaIteracyjnie(5);
+        System.out.println(silniaZ5);
+        int silniaZ6 = silnia.silniaIteracyjnie(6);
+        System.out.println("silniaZ6");
+    }
+}
---
+++ src/silnia.java
@@ -0,0 +1,16 @@
+public class silnia {
+
+    public static int silniaIteracyjnie(int liczba) {
+        int wynik = 1;
+        for (int i=2; i<=liczba; i++){
+            wynik = i * wynik;
+        }
+        return wynik;
+    }
+        public static int silniaRekurencyjnie(int liczba){
+            if (liczba == 1){
+                return 1;
+            }
+            return liczba * silniaRekurencyjnie(liczba -1);
+        }
+}
